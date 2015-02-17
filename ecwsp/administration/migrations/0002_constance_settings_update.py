2c5a531190d88a080c68db2612048c1c3aaa546a
Christina Levengood <fromageball@gmail.com>
2015-02-17 17:33:45 -0500

    added .first() method

--- ecwsp/administration/migrations/0002_constance_settings_update.py
+++ ecwsp/administration/migrations/0002_constance_settings_update.py
@@ -7,7 +7,7 @@ from constance import config
 def configuration_to_constance(apps, schema_editor):
     """adds Configuration object data to Constance Config in settings"""
     Configuration = apps.get_model("administration", "Configuration")  
-    configs = Configuration.objects.get(name="school pay rate per hour")
+    configs = Configuration.objects.filter(name="school pay rate per hour").first()
     formatted_config = configs.name.replace(" ", "_").upper
     config.formatted_config() = (configs.value, configs.help_text)
 
